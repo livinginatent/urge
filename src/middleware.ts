bc947d70e793cb87e1929ce31b3958aed65350b8
livinginatent <suleyman.eminbeyli@gmail.com>
2026-01-30 20:59:53 +0400

    Update middleware to exclude sitemap.xml and robots.txt from route protection: Enhance routing logic for better SEO and accessibility of static files.

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,10 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - sitemap.xml (sitemap file)
+     * - robots.txt (robots file)
      * - public files (public folder)
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
